8c230dce36e6da862c0b10f52ecd8cac6239ec66
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-03-02 02:19:27 +0530

    callstack

---
+++ callstack.js
@@ -0,0 +1,24 @@
+function ceo(){
+    console.log("Let's make a dent in the universe")
+    cto()
+}
+
+function cto(){
+    console.log("Let's make a gamechanging product")
+    vpengg()
+}
+
+function vpengg(){
+    console.log("Let's make a javascript framework")
+    techlead()
+}
+function techlead(){
+    console.log("Let's port angular to typescript")
+    developer()
+}
+
+function developer(){
+    console.log("Let's copy some code from stackoverflow")
+    throw new Error("the code didnt work out");
+}
+ceo()
