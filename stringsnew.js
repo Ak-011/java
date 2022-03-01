169e94e6f0fc797e287edede9011db797acf276e
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-03-01 23:13:45 +0530

    str

---
+++ stringsnew.js
@@ -0,0 +1,23 @@
+/*let p = 'ashish'
+let str = `this is a ${p}'s string`// can be write in both single and double quote
+let longStr = 'this is \
+a very long \
+string'
+console.log(longStr)
+console.log(longStr.length)
+//searching
+let s = 'this has some data here'
+let key = 'have'
+console.log(s.indexOf(key))
+let y = 'this is a easy job'
+let y1 = ' ab aab abbcd ba aab'
+console.log(y1.lastIndexOf('ab', 17))
+//console.log(y.indexOf('is', 3))*/
+
+let mainStr = "This-is-long-string"
+
+let smallStr = mainStr.substr(3,4)
+
+
+console.log(smallStr)
+
