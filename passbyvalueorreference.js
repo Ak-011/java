e46df24061df8b8f6255b8250b6dad70afe1bb99
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-03-02 01:45:49 +0530

    iife

---
+++ passbyvalueorreference.js
@@ -0,0 +1,13 @@
+var schoolWinner = ['Ashish', 'Negi', 'Binjola'];
+
+function changeWinner(winner){
+    winner[0] = "Draco";
+    winner[1] = "naancy"
+    winner[2] = "motu"
+    //console.log("Chnaged winner is "+ winner)
+}
+console.log("The winner is "+ schoolWinner);
+changeWinner(schoolWinner);
+console.log("The new winner is "+ schoolWinner);
+
+//in java script pass by value is applicable in this one
