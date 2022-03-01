2c2fe25525e96b69a43679f53a2b80d877885e16
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-03-02 00:19:51 +0530

    java

---
+++ array.js
@@ -0,0 +1,51 @@
+
+/*let arr = [1,2,3]
+console.log(arr[1])
+
+let arr2 = [1, 'hello', false, [2,3]]
+console.log(typeof arr2[0])
+//for(let i =0; i<arr2.length; i++){
+  //  console.log(arr2[i])
+//}
+
+//for(let val of arr2){
+  //  console.log(val)
+//}
+
+for(let index in arr2){
+    console.log(index)
+}
+*/
+//Add and remove items in array
+
+/*let fruits = ["aaple", "mango"]
+
+//push and pop
+fruits.push("melon", "apple")
+console.log(fruits)
+fruits.pop()
+
+console.log(fruits)
+
+//shift and unshift functions
+
+fruits.shift();
+console.log(fruits)
+
+fruits.unshift("kiwi", "grapes")
+console.log(fruits)*/
+
+//let notes = ['do','re', 'mi', 'fa', 'so', 'la', 'ti', 'do']
+let notes = ['do','re', 'mi']
+
+/*//console.log(notes.slice(4,6))
+//console.log(notes)
+let omit = notes.splice(4,2)
+console.log(omit)
+console.log(notes)*/
+
+//splice part 2
+//notes.splice(0,3, 'sa', 'ra','ga')
+//console.log(notes)
+console.log(notes.concat('sa','re','ga'))
+console.log(notes)
