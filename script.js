02881708d9f47e2718ac11c39b5528363ed0f81d
Ashish Kumar <44310112+Ak-011@users.noreply.github.com>
2022-03-06 11:23:02 +0530

    javaimp

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-let a = 10;
-let result = " ";
-for(let i =0; i<a; i++){
-    console.log(i)
+window.onload = function(){
+    let num = document.getElementById('num')
+    let list = document.getElementById('list')
+    let print = document.getElementById('print')
+
+    print.onclick = function(){
+
+        let start = new Date().getTime()
+
+        let N = parseInt(num.value)
+        let listHTMl = ''
+        for(let i =1; i<=N; i++ ){
+            listHTMl += '<li>'+ i + '</li>'
+        }
+        list.innerHTML = listHTMl
+        console.log(new Date().getTime()-start)
+    }
 }
